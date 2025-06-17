e4f651ab652f902ac355374b03e1d066fab3c6b8
Calliburn03 <kukretihimanshu03@gmail.com>
2025-06-17 16:23:24 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,17 @@
+import numbers
+
+print('Enter your first number')
+a=float(input())
+
+
+print('Enter your second number')
+b=float(input())
+
+
+print('functions')
+print('addition :   ', a+b)
+print('subtraction  : ', a-b)
+print('multiplication  : ', a*b)
+if b!=0: print('devision : ', a/b)
+else: print('division by zero not possible')
+
---
+++ task 2.py
@@ -0,0 +1,9 @@
+print('Enter your first name: ')
+a=(input())
+print('Enter your last name: ')
+b=(input())
+
+
+print("hello, {} {} , welcome to the world of python programming".format(a,b))
+
+
